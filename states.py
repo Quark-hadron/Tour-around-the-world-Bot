7040d5800ae385ebf14cf9fff12b018d1a3fd91d
輝き <163336015+Quark-hadron@users.noreply.github.com>
2025-06-04 20:42:11 +0400

    Add files via upload
    added inline buttons instead of regular ones + new system for saving information received from the user for subsequent analysis

---
+++ states.py
@@ -0,0 +1,10 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class TourSelection(StatesGroup):
+    selecting_people = State()
+    selecting_season = State()
+    selecting_tour_type = State()
+    neuro_search = State()
+
+class UserWishState(StatesGroup):
+    waiting_for_wish = State()
