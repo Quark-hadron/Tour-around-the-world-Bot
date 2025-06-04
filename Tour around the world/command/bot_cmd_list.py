a2c195c141a447277f34e6431df8d18a243bcbdb
輝き <163336015+Quark-hadron@users.noreply.github.com>
2025-06-04 20:43:53 +0400

    Delete Tour around the world directory

--- Tour around the world/command/bot_cmd_list.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import BotCommand
-
-private = [
-    BotCommand(command='about', description='О нас'),
-    BotCommand(command='start', description='Начать поиск заново')
-
-]
--- Tour around the world/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class TourSelection(StatesGroup):
-    selecting_people = State()
-    selecting_season = State()
-    selecting_tour_type = State()
-    neuro_search = State() 
